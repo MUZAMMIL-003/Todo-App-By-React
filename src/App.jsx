6134c0874db1ba9650b01c7e6b38b59637947c87
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-04-21 19:33:18 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       completed: false
       
     }]
-    // 
+  
     window.localStorage.setItem('todos', JSON.stringify(todoArr))
 
 //     const todos2 = JSON.parse(localStorage.getItem("todos"))
